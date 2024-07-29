e00229138374e717796abb18af0ce22418306dcd
NobidoNs <61043287+NobidoNs@users.noreply.github.com>
2024-07-29 12:16:35 +0500

    Add dinamic light on images

--- src/app/components/ProjectPage/Images/Tanks.jsx
+++ src/app/components/ProjectPage/Images/Tanks.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 
 export function TanksImage() {
 	const [style, setStyle] = useState({ transform: '' })
+	const [style2, setStyle2] = useState({ transform: '' })
 
 	const handleMouseMove = e => {
 		const rect = e.target.getBoundingClientRect()
@@ -12,8 +13,14 @@ export function TanksImage() {
 		const y = e.clientY - rect.top
 		const rotateX = (y - rect.height / 2) / 25
 		const rotateY = -(x - rect.width / 2) / 25
+		const w = rect.bottom - rect.top
+		const opac = 0.001 * (w - y)
 		setStyle({
 			transform: `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+		})
+		setStyle2({
+			transform: `rotate(${rotateY * 20}deg) translate(-50%, -50%)`,
+			opacity: opac,
 		})
 	}
 
@@ -21,18 +28,47 @@ export function TanksImage() {
 		setStyle({
 			transform: `perspective(1000px) rotateX(0deg) rotateY(0deg)`,
 		})
+		setStyle2({
+			transform: `rotate(180deg)`,
+			opacity: 0,
+		})
 	}
 
 	return (
-		<Image
-			className='object-fill hover:drop-shadow-[0_0_0.5rem_#60a5fa] rounded-3xl'
-			src='/Tanks/icon.png'
-			alt='Next.js Logo'
-			onMouseMove={handleMouseMove}
-			onMouseLeave={handleMouseLeave}
-			style={{ ...style, transition: 'transform 0.1s' }}
-			width={720}
-			height={80}
-		/>
+		<div className='w-full h-64 relative'>
+			<div
+				className='w-full h-64 absolute z-10 rounded-3xl'
+				style={{
+					...style,
+					transition: 'transform 0.1s',
+					overflow: 'hidden',
+					pointerEvents: 'none',
+				}}
+			>
+				<div
+					className='absolute opacity-0'
+					style={{
+						...style2,
+						top: '70%',
+						left: '70%',
+						pointerEvents: 'none',
+						backgroundImage:
+							'linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 100%)',
+						width: '880px',
+						height: '880px',
+						transformOrigin: '0% 0%',
+					}}
+				/>
+			</div>
+			<Image
+				className='object-fill hover:drop-shadow-[0_0_0.5rem_#60a5fa] rounded-3xl z-1 border-2 border-zinc-500'
+				src='/Tanks/icon.png'
+				alt='Tanks'
+				fill={true}
+				onMouseMove={handleMouseMove}
+				onMouseLeave={handleMouseLeave}
+				style={{ ...style, transition: 'transform 0.1s' }}
+			></Image>
+		</div>
 	)
 }
